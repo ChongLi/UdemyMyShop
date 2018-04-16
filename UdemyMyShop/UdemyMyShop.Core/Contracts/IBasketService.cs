cf8edb3f1c1a1d74110624771392782fc5edea04
ChongLi <epeebles@bruceoakley.com>
2018-04-16 10:18:06 -0500

    Added BasketService interface

---
+++ UdemyMyShop/UdemyMyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using UdemyMyShop.Core.ViewModels;
+
+namespace UdemyMyShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItmes(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+
+    }
+}
